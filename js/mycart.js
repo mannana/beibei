dcd00514a0f6a23be774f7a8e3ca81e981a28a62
manna <2351450114@qq.com>
2017-09-04 20:44:13 +0800

    commit

--- js/mycart.js
+++ js/mycart.js
@@ -33,25 +33,25 @@ $(function() {
 	$(".countbtn").click(function(){
 			var txt=$(this).html();
 			var content=parseInt($(this).parent().find(".countnum").val());
-			var price=$(this).parent().prev().find(".newprice").html()	
+			var price=$(this).parent().prev().find(".newprice").html()
 			switch(txt){
 				case "-":
 					content>1?$(this).parent().find(".countnum").val(--content):1;
 					break;
-				case "+":$(this).parent().find(".countnum").val(++content);
-				break;
+				case "+":
+					$(this).parent().find(".countnum").val(++content);
+					break;
 			}
 			var did=$(this).parent().data().id;
 			var dproid=$(this).parent().data().proid;
 			for(var i = 0; i < cookie.length; i++) {
-			if(did==cookie[i].id&&dproid==cookie[i].proid){	
-				cookie[i].count=content;
-				setCookie("info",JSON.stringify(cookie));
+				if(did==cookie[i].id&&dproid==cookie[i].proid){
+					cookie[i].count=content;
+					setCookie("info",JSON.stringify(cookie));
+				}
 			}
-			}
-			console.log($(this).parent().next().html())
 			$(this).parent().next().html(`${content*price}.00`)
-			
+			$(":checkbox").trigger("click")
 	});
 	//删除商品
 	$(".del").click(function(){
@@ -59,66 +59,81 @@ $(function() {
 		var dproid=$(this).data().proid;
 		var color=$(this).data().color;
 		var size=$(this).data().size;
-		if(confirm("确定要删除吗?")){	
+		if(confirm("确定要删除吗?")){
 			$(this).parent().parent().remove();
 			for(var i = 0; i < cookie.length; i++) {
-				if(did==cookie[i].id&&dproid==cookie[i].proid&&color==cookie[i].color&&size==cookie[i].size){	
+				if(did==cookie[i].id&&dproid==cookie[i].proid&&color==cookie[i].color&&size==cookie[i].size){
 					cookie.splice(i,1);
 					setCookie("info",JSON.stringify(cookie));
 				}
 			}
 		}
-		
+
 	});
 	//删除所有
 	$(".delAll").click(function(){
-		
-		if(confirm("确定要删除所有商品吗?")){	
+
+		if(confirm("确定要删除所有商品吗?")){
 			$(".listitem").html("");
 			setCookie("info","",-1);
 		}
 	});
-	
+
 	$(".selectAll").click(function(){
 		$(":checkbox").prop("checked",$(this).prop("checked"));
 		$(".chb").click();
 		$(".chb1").click();
 	})
-	
+
 	$(".chb").click(function(){
 		var parent=$(this).parent().parent();
 		parent.prev().find(".chb1").prop("checked",$(this).prop("checked"));
 		sum($(this));
+		for (var i = 0; i < $(".chb").length; i++) {
+			if(!$($(".chb")[i]).prop("checked")){
+				$(".selectAll").prop("checked",false);
+				return;
+			}else{
+				$(".selectAll").prop("checked",true);
+			}
+		}
 	});
 	$(".chb1").click(function(){
 		var parent=$(this).parent().parent();
 		parent.next().find(".chb").prop("checked",$(this).prop("checked"));
+		$(".selectAll").prop("checked",$(this).prop("checked"));
 		sum($(this));
-	
+		for (var i = 0; i < $(".chb1").length; i++) {
+			if(!$($(".chb1")[i]).prop("checked")){
+				$(".selectAll").prop("checked",false);
+				return;
+			}else{
+				$(".selectAll").prop("checked",true);
+			}
+		}
 	});
 	function sum(obj){
 		var parent=obj.parent().parent();
-		var total=0;	
+		var total=0;
 		if(obj.prop("checked")){
-			
 			var price=parseFloat(parent.parent().find(".newprice").html());
 			var count=parseFloat(parent.parent().find(".countnum").val());
-			parent.parent().find(".totalprice1").html(price*count+".00");		
+			parent.parent().find(".totalprice1").html(price*count+".00");
 		}else{
 			parent.parent().find(".totalprice1").html("0.00");
-			
+
 			$(".totalp").html(`￥0.00`);
 		}
 		$(".totalprice1").each(function(){
-				total+=parseFloat($(this).html());
-				if(total>0){
-					$(".confirm").addClass("active");
-				}else{
-					$(".confirm").removeClass("active");
-				}
-			});
-			$(".totalp").html(`￥${total}.00`);
-		
-		
+			total+=parseFloat($(this).html());
+			if(total>0){
+				$(".confirm").addClass("active");
+			}else{
+				$(".confirm").removeClass("active");
+			}
+		});
+		$(".totalp").html(`￥${total}.00`);
+
+
 	}
 })
